8f1eb6f6489e5a2f132ac1ae52ebe547be6ff817
Sab Gibson <sabelogibson1@gmail.com>
2023-10-30 13:50:20 +0000

    created attention block

--- model.py
+++ model.py
@@ -92,13 +92,66 @@ class LayerNormalization(nn.Module):
 
 class PointwiseFeedForwardBlock(nn.Module):
     def __init__(self,d_model:int,d_ff:int,dropouot:float):
+        """ Feedforward Neural Network"""
         super.__init()
         self.linear = nn.Linear(d_model,d_ff) #w1 and b1
         self.dropout = nn.Dropout(dropouot)
-        self.linear_2 = nn.Linear(dff,d_model) # w2 and b2 
+        self.linear_2 = nn.Linear(d_ff,d_model) # w2 and b2 
 
 
     def forward(self,x):
         # (Batch , seq_len , d_model) -> (Batch,seq_len,d_ff) -> (Batch , seq_len , d_model)
         return self.linear_2(self.dropout(F.relu(self.linear(x))))
+
+
+
+class MultiHeadAttentionBlock(nn.Module):
+    def __init__(self,d_model:int, h:int,dropout:float) -> None:
+        """ 
+        Multi head attention module for batched inputs
+
+        """
+        super().__init__()
+        self.d_model = d_model
+        assert d_model % h == 0 , "d_model must be divisable by h"
+        self.h = h
+        self.dk = d_model // h 
+        self.w_q = nn.Linear(d_model,d_model) # wq
+        self.w_k = nn.Linear(d_model,d_model) # wk
+        self.w_v = nn.Linear(d_model,d_model) # wv
+        self.w_o = nn.Linear(d_model,d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    @staticmethod
+    def attention(query,key,value,mask,dropout:nn.Dropout):
+        d_k = query.shape[-1]
+
+        attention_scores = torch.einsum("bhsd,bhtd->bhst", [query, key]) / math.sqrt(d_k)
+        if mask is not None:
+            attention_scores.masked_fill_(mask == 0 , -1e9)
+        attention_scores = attention_scores.softmax(dim=-1) # (Batch,h,seq_len,seq_len)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+        return  torch.einsum("bhst,bhtd->bhsd", attention_scores, value) , attention_scores
+
+    def forward(self,q,k,v,mask):
+        query = self.w_q(q) #(Batch, seq_len, d_model)
+        key = self.w_k(k) #(Batch, seq_len, d_model)
+        value = self.w_v(v) #(Batch, seq_len, d_model)
+
+        # (Batch, seq_len, d_model) -> (Batch, seq_len, h, d_k) -> (Batch,h, seq_len, d_k) each head gets full sentence but part of embedding
+        query = query.view(query.shape[0],query.shape[1],self.h,self.dk).transpose(1,2)
+        key = key.view(key.shape[0],key.shape[1],self.h,self.dk).transpose(1,2)
+        value = query.view(value.shape[0],value.shape[1],self.h,self.dk).transpose(1,2)
+
+        x , self.attention_scores = MultiHeadAttentionBlock.attention(query,key,value,mask,self.dropout)
+
+        # (Batch,h,seq_len,d_k) --> (Batch,seq_len,h,d_k) --> (Batch,seq_len,d_model)
+        x = x.transpose(1,2).contiguous().view(x.shape[0],-1,self.h*self.dk)
+
+        # (Batch,seq_len,d_model) -- > (Batch,seq_len,d_model)
+        return self.w_o(x)
+
+
+
 
