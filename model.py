03c4685b502e92fcf73859967f0b35951a006146
Sab Gibson <sabelogibson1@gmail.com>
2023-10-30 12:08:56 +0000

    added feedforward block

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import math
 
 class InputEmbeddings(nn.Module):
@@ -66,4 +67,38 @@ class PositionalEncoding(nn.Module):
     def forward(self,x):
         x = x + (self.pe[:,x.shape[1],:]).requires_grad_(False)
         return self.dropout(x)
+
+class LayerNormalization(nn.Module):
+
+    def __init__(self,eps : float = 10e-6) -> None:
+        """ 
+        Layer norm will normalize each feature with respect to itself 
+
+        parameters
+        ----------
+
+
+        """
+        super.__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1))
+        self.bias = nn.Parameter(torch.zeros(1))
+
+    def forward(self,x):
+        mean = x.mean(di = -1,keepdim=True)
+        std = x.std(dim=-1,keepdim=True)
+        return self.alpha * (x - mean)/ (std + self.eps) * self.bias
+    
+
+class PointwiseFeedForwardBlock(nn.Module):
+    def __init__(self,d_model:int,d_ff:int,dropouot:float):
+        super.__init()
+        self.linear = nn.Linear(d_model,d_ff) #w1 and b1
+        self.dropout = nn.Dropout(dropouot)
+        self.linear_2 = nn.Linear(dff,d_model) # w2 and b2 
+
+
+    def forward(self,x):
+        # (Batch , seq_len , d_model) -> (Batch,seq_len,d_ff) -> (Batch , seq_len , d_model)
+        return self.linear_2(self.dropout(F.relu(self.linear(x))))
 
